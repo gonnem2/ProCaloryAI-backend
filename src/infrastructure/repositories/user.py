8a3cfb0e14f57bbb84aa5bf5f4821a58e698c4fe
andrejkamenskij <gonne.minecraft@mail.ru>
2026-03-03 14:41:29 +0300

    add: jeneric to abstractRepo

--- src/infrastructure/repositories/user.py
+++ src/infrastructure/repositories/user.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.domain.models import User
 from src.infrastructure.repositories import AbstractRepository
 
 
-class UserRepository(AbstractRepository):
-
+class UserRepository(AbstractRepository[User]):
     def __init__(self, session: AsyncSession):
         self.session: AsyncSession = session
